fd84295804d3a0dac511f608a9d27e341657e9d0
Nayan Mondal <n007kingkong@gmail.com>
2026-07-11 23:09:01 +0600

    change the logo and another details of footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -7,12 +7,12 @@ import { useNavigate } from 'react-router';
 
 export default function Footer() {
 
-    const { settings } = useContext(SettingsContext);
-     const [isScrolled, setIsScrolled] = useState(false);
-     const {address,brandName,copyrightText,domain,email,facebookUrl,favIcon,footerAboutText,instagramUrl,linkedinUrl,logo,pinterestUrl,title,twitterUrl,workingHours,youtubeUrl
-}= settings || {}
+  const { settings } = useContext(SettingsContext);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const { address, brandName, email, facebookUrl, favIcon, footerAboutText, instagramUrl, linkedinUrl, logo, twitterUrl, workingHours, youtubeUrl
+  } = settings || {}
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   // Scroll handler for background color change
   useEffect(() => {
@@ -28,26 +28,25 @@ export default function Footer() {
       <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-8 mb-8">
         <div>
           {
-            settings ? 
-            <div className='flex gap-2 text-white'>
-              <img className='h-8 w-10' src={logo} alt="" /> 
-              <span className="font-serif font-normal italic tracking-wide">{brandName}</span>
-            </div>
-            : 
-            <div 
-            className={`flex items-center gap-2.5 font-sans font-black text-2xl tracking-tight cursor-pointer select-none transition-transform duration-300 active:scale-95 ${
-              isScrolled ? 'text-blue-600' : 'text-white'
-            }`} 
-            onClick={() => { navigate('/');  }}
-          >
-            <Home className={`h-6 w-6 transition-colors duration-300 ${isScrolled ? 'text-blue-600' : 'text-blue-400'}`} />
-            <span className="font-serif font-normal italic tracking-wide">Prime<span className="font-sans font-black not-italic tracking-tight">Estates</span></span>
-          </div>
+            settings ?
+              <div className='flex gap-2 text-white'>
+                <img className='h-8 w-10' src={logo} alt="" />
+                <span className="font-serif font-normal italic tracking-wide">{brandName}</span>
+              </div>
+              :
+              <div
+                className={`flex items-center gap-2.5 font-sans font-black text-2xl tracking-tight cursor-pointer select-none transition-transform duration-300 active:scale-95 ${isScrolled ? 'text-blue-600' : 'text-white'
+                  }`}
+                onClick={() => { navigate('/'); }}
+              >
+                <Home className={`h-6 w-6 transition-colors duration-300 ${isScrolled ? 'text-blue-600' : 'text-blue-400'}`} />
+                <span className="font-serif font-normal italic tracking-wide">Prime<span className="font-sans font-black not-italic tracking-tight">Estates</span></span>
+              </div>
           }
           {
-            settings? <p className="text-sm">{footerAboutText}</p>
-            :
-            <p className="text-sm">This is PrimeEstate . A modern and premium properties supplier real estate corporation</p>
+            settings ? <p className="text-sm">{footerAboutText}</p>
+              :
+              <p className="text-sm">This is PrimeEstate . A modern and premium properties supplier real estate corporation</p>
           }
         </div>
         <div>
@@ -61,45 +60,54 @@ export default function Footer() {
         <div>
           <h4 className="text-white font-semibold mb-4">Contact Info</h4>
           {
-            settings? <p className="text-sm mb-2">{address}</p> 
-            :
-            <p className="text-sm mb-2">123 Luxury Way, Suite 100<br/>Beverly Hills, CA 90210</p>
+            settings ? <p className="text-sm mb-2">{address}</p>
+              :
+              <p className="text-sm mb-2">123 Luxury Way, Suite 100<br />Beverly Hills, CA 90210</p>
           }
           {
-            settings?<p className="text-sm">{email}</p>
-            :
-            <p className="text-sm">support@primeestates.com</p>
+            settings ? <p className="text-sm">{email}</p>
+              :
+              <p className="text-sm">support@primeestates.com</p>
           }
         </div>
-        <div>
+        <div >
           <h4 className="text-white font-semibold mb-4">Follow Us</h4>
-          <div className="flex gap-4">
-            {
-              settings? <a href={facebookUrl} className="hover:text-white transition"><FaFacebook size={20}/></a>
-              :
-              <a href="#" className="hover:text-white transition"><FaFacebook size={20}/></a>
-            }
-            {
-              settings? <a href={twitterUrl} className="hover:text-white transition"><FaTwitter size={20}/></a>
-              :
-              <a href="#" className="hover:text-white transition"><FaTwitter size={20}/></a>
-            }
-            {
-              settings? <a href={instagramUrl} className="hover:text-white transition"><FaInstagram size={20}/></a>
-              :
-              <a href="#" className="hover:text-white transition"><FaInstagram size={20}/></a>
-            }
-            {
-              settings? <a href={linkedinUrl} className="hover:text-white transition"><FaLinkedin size={20}/></a>
-              :
-              <a href="#" className="hover:text-white transition"><FaLinkedin size={20}/></a>
-            }
-            
+          <div className='flex  gap-2 flex-col'>
+            <div className="flex gap-4">
+              {
+                settings ? <a href={facebookUrl} className="hover:text-white transition"><FaFacebook size={20} /></a>
+                  :
+                  <a href="#" className="hover:text-white transition"><FaFacebook size={20} /></a>
+              }
+              {
+                settings ? <a href={twitterUrl} className="hover:text-white transition"><FaTwitter size={20} /></a>
+                  :
+                  <a href="#" className="hover:text-white transition"><FaTwitter size={20} /></a>
+              }
+              {
+                settings ? <a href={instagramUrl} className="hover:text-white transition"><FaInstagram size={20} /></a>
+                  :
+                  <a href="#" className="hover:text-white transition"><FaInstagram size={20} /></a>
+              }
+              {
+                settings ? <a href={linkedinUrl} className="hover:text-white transition"><FaLinkedin size={20} /></a>
+                  :
+                  <a href="#" className="hover:text-white transition"><FaLinkedin size={20} /></a>
+              }
+
+            </div>
+            <div className='text-green-400 text-sm'>
+              {
+                settings ? <p>{workingHours}</p>
+                  :
+                  <p>Open : Monday to Friday from 9 AM to 10 PM</p>
+              }
+            </div>
           </div>
         </div>
       </div>
       <div className="max-w-7xl mx-auto border-t border-gray-800 pt-6 text-center text-xs">
-        <p>&copy; {new Date().getFullYear()} PrimeEstates. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} {brandName} All rights reserved.</p>
       </div>
     </footer>
   );
