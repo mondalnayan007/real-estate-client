3932b2246b49b3e51b1e9b404901199d6aa1da4b
Nayan Mondal <n007kingkong@gmail.com>
2026-07-30 23:12:33 +0600

    changing the front end theme design

--- src/components/OurVision.jsx
+++ src/components/OurVision.jsx
@@ -9,19 +9,19 @@ export default function OurVision() {
   console.log(user);
 
   return (
-    <section className="w-full bg-[#185F35] text-white py-16 transition-colors duration-300">
+    <section className="w-full text-[#185F35] bg-white py-16 transition-colors duration-300">
       <div className="max-w-7xl mx-auto space-y-12">
         
         {/* ================= 🌟 HEADER SECTION ================= */}
         <div className="text-center max-w-4xl mx-auto space-y-4">
           
           {/* Main Title */}
-          <h1 className="text-3xl sm:text-4xl md:text-5xl font-black tracking-wider uppercase leading-tight text-white">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl font-black tracking-wider uppercase leading-tight ">
             YOUR DREAM HOME AWAITS
           </h1>
 
           {/* Subtitle / Description */}
-          <p className="text-sm sm:text-base text-slate-300 font-normal leading-relaxed max-w-3xl mx-auto">
+          <p className="text-sm sm:text-base text-[#2c8f55] font-normal leading-relaxed max-w-3xl mx-auto">
            The {user? `${user.agencyName}` : 'PrimeEstate'} is a visionary real estate company dedicated to creating modern, 
             community-focused living spaces. With a commitment to affordability, transparency, and smart 
             innovation, we design homes where families feel secure, connected, and proud to belong.
