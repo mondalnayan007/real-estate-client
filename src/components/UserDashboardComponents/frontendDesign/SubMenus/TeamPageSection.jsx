a82caa30228e62ab31e53b77c76ea9a4ed7339ba
Nayan Mondal <n007kingkong@gmail.com>
2026-07-31 01:11:34 +0600

    created the team page customization option

--- src/components/UserDashboardComponents/frontendDesign/SubMenus/TeamPageSection.jsx
+++ src/components/UserDashboardComponents/frontendDesign/SubMenus/TeamPageSection.jsx
@@ -45,7 +45,7 @@ export default function TeamManagementSection() {
     setFetching(true);
     try {
       // 👈 আপনার ব্যাকএন্ডের GET API URL এখানে বসান
-      const response = await fetch('http://localhost:5000/api/v1/team-members'); 
+      const response = await fetch(`http://localhost:4000/api/admin/team-members?${user?.agentId}`); 
       const result = await response.json();
 
       if (response.ok) {
