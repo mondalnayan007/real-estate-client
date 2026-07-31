bf3384fcd24f130df8b8070e75abd39d5dbc8abd
Nayan Mondal <n007kingkong@gmail.com>
2026-08-01 00:50:18 +0600

    redesign the homepage projects design

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import { Outlet } from 'react-router';
 import Footer from '../Footer';
+import { Mail, Phone, MessageCircle } from 'lucide-react';
+import ContactFab from '../components/ContactFab';
 
 const Root = () => {
     return (
@@ -11,16 +13,7 @@ const Root = () => {
                 <Outlet></Outlet>
             </div>
             <Footer></Footer>
-            <div className="fab fab-flower">
-
-                <div tabIndex={0} role="button" className="btn btn-lg btn-circle btn-primary relative bottom-10 right-10">F</div>
-
-                {/* buttons that show up when FAB is open */}
-                <button className="btn btn-lg btn-circle">A</button>
-                <button className="btn btn-lg btn-circle">B</button>
-                <button className="btn btn-lg btn-circle">C</button>
-                <button className="btn btn-lg btn-circle">D</button>
-            </div>
+           <ContactFab></ContactFab>
         </div>
     );
 };
